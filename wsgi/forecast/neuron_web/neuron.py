838c0f0d575dcbb6f6fd0cf372f5ec88b740ebf4
Alexey Kutepov <alkutepov@gmail.com>
2014-11-24 17:26:16 +0400

    Added neuron

---
+++ wsgi/forecast/neuron_web/neuron.py
@@ -0,0 +1,29 @@
+__author__ = 'Alexey Kutepov'
+
+import math
+
+# Класс описывает нейрон
+class Neuron:
+    def __init__(self, a=0.5):
+        super().__init__()
+        self.a = a
+
+    # Вычисляет текущее состояние нейрона
+    def _state(self, x, weights):
+        state = 0
+        for i in range(len(x)):
+            state += x[i] * weights[i]
+        return state
+
+    # Функция состояния нейрона
+    def _f(self, state):
+        return 1 / math.exp(- self.a * state)
+
+    # Активирует нейрон и возвращает результирующее значение
+    def action(self, x, weights):
+        if not x is list or not weights is list:
+            raise AttributeError("ERROR: x or weights is not list")
+        if len(x) != len(weights):
+            raise ValueError("ERROR: len(x) != len(weights)")
+        state = self._state(x, weights)
+        return self._f(state)
